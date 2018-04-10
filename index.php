9ef15ac0e89556486e25427fdb786a22bed4594e
Okeke Felix <okekefelix1@gmail.com>
2018-04-10 17:42:39 +0100

    Added index files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG-1</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<header>
+		HNG INTERNSHIP 2018
+	</header>
+
+	<div  id="time">
+		<h4>TIME IS <?php
+					 date_default_timezone_set("Africa/Lagos"); 
+					echo date("g:i a", time());
+					?> 
+		</h4>
+	</div>
+</body>
+</html>
+
